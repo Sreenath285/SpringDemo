84da1530fe3f1d8d199db2559d5178af603489ca
Sreenath285 <sreenath.285@gmail.com>
2022-05-19 13:45:36 +0530

    Displaying hello using REST controller

--- src/main/java/com/example/springdemo/controller/SpringRestController.java
+++ src/main/java/com/example/springdemo/controller/SpringRestController.java
@@ -1,6 +1,5 @@
 package com.example.springdemo.controller;
 
-import com.example.springdemo.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,25 +8,5 @@ public class SpringRestController {
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello () {
         return "Hello from Sreenath";
-    }
-
-    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-    public String sayHello(@RequestParam(value = "name") String name) {
-        return "Hello " + name + " !";
-    }
-
-    @GetMapping("/params/{name}")
-    public String sayHelloParam(@PathVariable String name) {
-        return "Hello " + name + " !";
-    }
-
-    @PostMapping("/post")
-    public String sayHello(@RequestBody User user) {
-        return "Hello " + user.getFirstName() + " " + user.getLastName() + " !";
-    }
-
-    @PutMapping("/put/{firstName}")
-    public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
-        return "Hello " + firstName + " " + lastName + " !";
     }
 }
